9882649b95da586f9b5d2358788e6ce57a9085c4
Kelly Innes <kinnes@azavea.com>
2017-11-06 17:56:14 -0500

    Implement region list -> details view animation
    - adapt region list -> region details view animation from prototype

--- src/js/App.js
+++ src/js/App.js
@@ -16,6 +16,7 @@ window.App = Backbone.View.extend({
 
         this.emptyDetailsView = this.emptyDetailsView.bind(this);
         this.fadeInListView = this.fadeInListView.bind(this);
+        this.animateDetailsViewReplacingList = this.animateDetailsViewReplacingList.bind(this);
         this.toggleDetails = this.toggleDetails.bind(this);
         this.slideOutStatsRegion = _.once(this.slideOutStatsRegion).bind(this);
 
@@ -50,18 +51,67 @@ window.App = Backbone.View.extend({
     },
 
     fadeInListView: function() {
+        this.regionListView = new RegionListView({ model: this.model });
+        this.regionListView.render();
         this.regionListView.$el.fadeIn(200);
+    },
+
+    animateDetailsViewReplacingList: function() {
+        var self = this;
+        var selectedRegion = this.model.get('selectedRegion').get('title');
+        var listItems = $('#region-location-list').children();
+        var $sidebarInnerEl = $('.sidebar--inner');
+        var promise = $.Deferred();
+        var $selectedListItem;
+
+        promise.then(function() {
+            self.regionListView.$el.hide();
+            self.regionListView.$el.empty();
+            self.regionDetailsView.render();
+            self.regionDetailsView.$el.show();
+        });
+
+        $.each(listItems, function(index, listItem) {
+            if (listItem.id !== selectedRegion) {
+                // `delay` value is `200 * index` to stagger the fadeouts for
+                // each list item rather than fading them all simultaneously.
+                // `fadeTo` duration specifies that each fadeout will happen
+                // over 300 ms rather than instantaneously.
+                $(listItem).delay(200 * index).fadeTo(300, 0);
+            } else {
+                $selectedListItem = $(listItem);
+            }
+        });
+
+        // If $selectedListItem's unset, toggle views without animating
+        if (!$selectedListItem) {
+            promise.resolve();
+            return _.noop();
+        }
+
+        $selectedListItem
+            .delay(listItems.length * 200)
+            .queue(function(next) {
+                var top = $sidebarInnerEl.offset().top - $selectedListItem.offset().top;
+                $selectedListItem.animate({
+                    top: top + 'px',
+                    margin: '0 -3rem 0 -3rem'
+                }, 500, promise.resolve).fadeOut(500);
+                next();
+            });
+
+        // Returning a noop here to ensure that this function has the same
+        // return type whether or not it executes the animation.
+        return _.noop();
     },
 
     toggleDetails: function() {
         if (this.model.get('detailsVisible')) {
-            this.regionListView.$el.hide();
             this.regionDetailsView = new RegionDetailsView({
                 model: this.model.get('selectedRegion'),
                 hideDetails: this.model.hideDetails,
             });
-            this.regionDetailsView.render();
-            this.regionDetailsView.$el.show();
+            this.animateDetailsViewReplacingList();
         } else {
             this.regionDetailsView.$el.fadeOut(200);
             _.delay(this.emptyDetailsView, 200);
